b71e2b266f0c32f8c26d1d755ba2cdc6207c451d
Jakub Sokołowski <VerdancyOfficial@gmail.com>
2022-06-23 12:13:55 +0200

    the main add

---
+++ config.production.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'baseUrl' => 'https://php-ml.org',
+    'production' => true,
+
+    // DocSearch credentials
+    'docsearchApiKey' => '',
+    'docsearchIndexName' => '',
+];
